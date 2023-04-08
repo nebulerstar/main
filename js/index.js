01613a3804173b4028ac93fb602667b984538452
Neutrino Star <nebulerstar@gmail.com>
2023-04-08 02:57:15 -0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,1 +1,27 @@
+import * as THREE from 'three';
+function init() {
+    
+    let scene = new THREE.Scene();
+    
+    let camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 100);
+    
+    camera.position.set(0, 0, 10);
+    
+    const sizes = {
+        width: window.innerWidth,
+        height: window.innerHeight,
+    }
+    let renderer = new THREE.WebGLRenderer();
+    renderer.setSize(sizes.width, sizes.height);
+    
+    document.body.appendChild(renderer.domElement);
 
+    var clock = new THREE.Clock();
+    
+    function tick() {
+        requestAnimationFrame(tick);
+        renderer.render(scene, camera);
+    }
+    tick();
+}
+init();
